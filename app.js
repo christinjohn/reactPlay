9be20c3130de699e1d7b04f57640e753c0fba849
Christin Padinjareveettil <christinjohn.yb@gmail.com>
2023-06-18 17:34:17 +0530

    Initial changes

---
+++ app.js
@@ -0,0 +1,12 @@
+const parent = React.createElement('div', {id: 'parent'}, 
+  React.createElement('div', {id: 'child'}, [
+    React.createElement('h1', {}, 'h1 tag'),
+    React.createElement('h2', {}, 'h2 tag')
+  ])
+)
+
+const heading = React.createElement('h1', {
+  id: 'heading'
+}, 'hello world from react');
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent);
